efd3ec05f594d14793a1352ccac295927a1a08e4
John Nunemaker <nunemaker@gmail.com>
2015-01-31 17:20:09 -0500

    Remove events from stars

--- test/controllers/stars_controller_test.rb
+++ test/controllers/stars_controller_test.rb
@@ -3,8 +3,6 @@ require 'test_helper'
 class StarsControllerTest < ActionController::TestCase
   test "should get index" do
     sign_in users(:jnunemaker)
-    @controller.github_client.
-      expects(:received_events).with("jnunemaker").returns([])
     get :index
     assert_response :success
   end
